4781bb03c0d9a12ca99237022fb4b200282dfa66
Cavsteeek <uzochukwuikemdinachi@gmail.com>
2024-09-08 00:16:30 +0100

    basic operations with api methods

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
 @app.get("/")
-def get_user():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "FastAPI World"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your post"}
+
+
+@app.post("/create_post")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']}, content: {payload['body']}"}
