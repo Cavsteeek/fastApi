1a5346c918c6c2efca2bb226ad12e04730e591b5
Cavsteeek <uzochukwuikemdinachi@gmail.com>
2024-09-12 15:54:21 +0100

    basic crud operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from random import randrange
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response, status
 from fastapi.params import Body
 from pydantic import BaseModel
 
@@ -13,6 +14,24 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = [
+    {"title": "title of post", "body": "body of post", "id": 1},
+    {"title": "Favourite food", "body": "Pizza", "id": 2},
+]
+
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+
 @app.get("/")
 def root():
     return {"message": "FastAPI World"}
@@ -20,10 +39,38 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your post"}
+    return {"data": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    # print(post.published)
-    return {post.title, post.body, post.published, post.rating}
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 100000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+# @app.get("/posts/latest")
+# def get_latest_post():
+#     post = my_posts[len(my_posts) - 1]
+#     return {"detail": post}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found",
+        )
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"post with id: {id} was not found"}
+    return {"post_detail": post}
+
+
+@app.delete("/post/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
