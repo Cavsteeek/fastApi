2cb6cd8c8b4e6339e1de9208205e4237e251f81f
Cavsteeek <uzochukwuikemdinachi@gmail.com>
2024-10-12 16:37:05 +0100

    just for reference

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from random import randrange
-from typing import Optional
+from typing import Optional, List
 from fastapi import Depends, FastAPI, HTTPException, Response, status
 from fastapi.params import Body
 import psycopg2
@@ -9,12 +9,13 @@ from . import models, schemas
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-# ROUTES
+# Post Endpoints
 
 
 @app.get("/")
@@ -22,29 +23,23 @@ def root():
     return {"message": "FastAPI World"}
 
 
-# @app.get("/sql")
-# def test(db: Session = Depends(get_db)):
-#     posts = db.query(models.Post).all
-#     return {"data": posts}
-
-
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return new_post
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     print(post)
@@ -54,7 +49,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} was not found",
         )
-    return {"post_detail": post}
+    return post
 
 
 @app.delete("/post/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -73,6 +68,34 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}")
-def update_post(id: int):
-    return {"data": "updated_post"}
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(
+    id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)
+):
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+
+    if post == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} does not exist",
+        )
+
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+
+    return post_query.first()
+
+
+# User Endpoints
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,35 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+# Posts Schema
 
 
-class Post(BaseModel):
+class PostBase(BaseModel):
     title: str
     content: str
     published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+
+# User Schema
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class User(BaseModel):
+    email: str
+
+
+class Config:
+    orm_mode = True
